fc6f18150a14c766eaea6202e9dfe7b550cf21b4
pe5 <eero.perola@helsinki.fi>
2020-05-13 23:38:24 +0300

    tes

---
+++ data/Korit_2.js
@@ -0,0 +1,26 @@
+var json_Korit_2 = {
+"type": "FeatureCollection",
+"name": "Korit_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "title": "Kori 10" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.426253822014548, 60.467946834606238 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 9" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.425323241573842, 60.466584366213326 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 8" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.425799, 60.465812 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 7" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.427524792912109, 60.465993185860455 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 6" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.428311005056553, 60.465935482408369 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 5" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.428290966984264, 60.467065605382288 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 4" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.428896, 60.466595 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 3" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.429985952544406, 60.466486787670881 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 2" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.430660904165357, 60.467037568498746 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 1" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.431025657919744, 60.466581722616311 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 19" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.432791049336902, 60.466749882942054 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 18" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.431989305914005, 60.468092918088615 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 17" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.430293, 60.468266 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 16" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.428627, 60.468485 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 15" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.425841720542877, 60.469038404154375 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 14" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.425374, 60.469967 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 13" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.423620374225923, 60.472439674059935 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 12" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.424902, 60.473527 ] ] } },
+{ "type": "Feature", "properties": { "title": "Kori 11" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.426071, 60.469528 ] ] } }
+]
+}
---
+++ data/Tiit_3.js
@@ -0,0 +1,26 @@
+var json_Tiit_3 = {
+"type": "FeatureCollection",
+"name": "Tiit_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "title": "Tii 10" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.425513348752329, 60.467044426737473 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 8" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.426845, 60.465443 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 9" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.427198310886887, 60.466097803956366 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 7" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.429234, 60.465972 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 6" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.428428932766618, 60.466541662346316 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 5" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.428235925274713, 60.466521697144763 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 4" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.430359, 60.466626 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 3" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.430826036387497, 60.467045657200593 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 2" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.430937853591377, 60.466588616561666 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 1" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.432517058894508, 60.466522432026416 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 19" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.432780925341621, 60.467519861796355 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 18" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.430879708895525, 60.468438226446075 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 17" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.429086388254785, 60.46840951973234 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 16" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.427312721771475, 60.467938237146996 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 15" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.424245099005919, 60.469642757644756 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 14" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.4238201783634, 60.470472324115811 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 13" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.424361828359395, 60.473077321398556 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 12" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.423227662437167, 60.47339131692911 ] ] } },
+{ "type": "Feature", "properties": { "title": "Tii 11" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 27.426831204972082, 60.468240399758528 ] ] } }
+]
+}
